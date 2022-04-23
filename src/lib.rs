e4de18fabf2fc7b47bb8653d33517562ffb49086
Mado Nanika <nanikamado@gmail.com>
2022-04-23 19:39:09 +0900

    ジャッジサーバーに回答を送れるようにする

---
+++ src/lib.rs
@@ -0,0 +1,101 @@
+use futures_util::{SinkExt, StreamExt};
+use serde::Deserialize;
+use serde_json::json;
+use tokio::{io::AsyncWriteExt, net::TcpStream, sync::mpsc::Sender};
+use tokio_tungstenite::{
+    connect_async, tungstenite::protocol::Message, MaybeTlsStream, WebSocketStream,
+};
+
+#[derive(Debug, Deserialize)]
+#[serde(tag = "type", rename_all(deserialize = "snake_case"))]
+pub enum ReternMessage {
+    SubmissionResult {
+        test_case_number: usize,
+        result: bool,
+        time: u64,
+        killed: bool,
+    },
+    CodetestResult {
+        result: Vec<u8>,
+        time: u64,
+    },
+    NumberOfTestCases {
+        n: usize,
+    },
+    Close,
+    NotSuchProblem {
+        problem_number: usize,
+    },
+}
+
+pub async fn submit(
+    lang: String,
+    problem_number: usize,
+    code: String,
+    mut ws_stream: WebSocketStream<MaybeTlsStream<TcpStream>>,
+    sender: Sender<ReternMessage>,
+) {
+    ws_stream
+        .send(Message::Text(
+            json!({
+                "type": "submission",
+                "lang": lang,
+                "problem_number": problem_number,
+                "code": code,
+            })
+            .to_string(),
+        ))
+        .await
+        .unwrap();
+    ws_stream
+        .for_each(|message| async {
+            match message.unwrap() {
+                Message::Text(message) => {
+                    let data = serde_json::from_str(&message).unwrap();
+                    sender.send(data).await.unwrap();
+                }
+                Message::Close(_) => {
+                    let _ = sender.send(ReternMessage::Close).await;
+                }
+                _ => panic!(),
+            }
+        })
+        .await;
+}
+
+pub async fn codetest(
+    lang: String,
+    code: String,
+    input: Option<Vec<u8>>,
+    mut ws_stream: WebSocketStream<MaybeTlsStream<TcpStream>>,
+) {
+    ws_stream
+        .send(Message::Text(
+            json!({
+                "type": "codetest",
+                "lang": lang,
+                "code": code,
+                "input" : input.map(base64::encode),
+            })
+            .to_string(),
+        ))
+        .await
+        .unwrap();
+    ws_stream
+        .for_each(|message| async {
+            let message = message.unwrap();
+            if let Message::Text(data) = message {
+                let v: serde_json::map::Map<_, _> = serde_json::from_str(&data).unwrap();
+                tokio::io::stdout()
+                    .write_all(&base64::decode(v["result"].as_str().unwrap()).unwrap())
+                    .await
+                    .unwrap();
+            }
+        })
+        .await;
+}
+
+pub async fn connect_to_server(server_address: &str) -> WebSocketStream<MaybeTlsStream<TcpStream>> {
+    let url = url::Url::parse(&format!("ws://{}:5620", server_address)).unwrap();
+    connect_async(url).await.expect("Failed to connect").0
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use clap::{Parser, Subcommand};
-use json::object;
-use std::{fs::read_to_string, io::Read, process::exit};
+use erase_output::Erase;
+use futures_util::future;
+use mmagolf::{codetest, connect_to_server, submit, ReternMessage};
+use std::{fmt::Display, fs::read_to_string, io::Read, iter, process::exit};
+use termion::{color, style};
+use tokio::sync::mpsc::{channel, Receiver};
 
 #[derive(Debug, Parser)]
 #[clap(version, about, long_about = None)]
@@ -36,9 +40,10 @@ enum Commands {
     },
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let args = Cli::parse();
-
+    let server_address = std::env::var("MMA_GOLF_SERVER").unwrap_or("atlas".to_string());
     match args.command {
         Commands::Submit {
             code,
@@ -49,15 +54,11 @@ fn main() {
                 eprintln!("{}: {}", code, e);
                 exit(1)
             });
-            println!(
-                "{}",
-                object! {
-                    "type": "submit",
-                    "lang": lang,
-                    "problem-number": problem_number,
-                    "code": code,
-                }
-            )
+            let ws_stream = connect_to_server(&server_address);
+            let (sender, receiver) = channel(100);
+            let submission = submit(lang, problem_number, code, ws_stream.await, sender);
+            let display_result = display_result(receiver);
+            future::join(submission, display_result).await;
         }
         Commands::Codetest { code, lang } => {
             let code = read_to_string(&mut code.clone()).unwrap_or_else(|e| {
@@ -78,15 +79,153 @@ fn main() {
                     input
                 })
             };
-            println!(
-                "{}",
-                object! {
-                    "type": "codetest",
-                    "lang": lang,
-                    "code": code,
-                    "input": input.map(base64::encode),
-                }
-            );
+            let ws_stream = connect_to_server(&server_address).await;
+            codetest(lang, code, input, ws_stream).await;
         }
     }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+#[allow(unused)]
+enum JudgeStatus {
+    Ac(u64),
+    // Mle(u64),
+    Tle(u64),
+    // Re,
+    // Ole,
+    // Ie,
+    Wa(u64),
+    Wj,
+}
+
+impl Display for JudgeStatus {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            JudgeStatus::Ac(t) => write!(
+                f,
+                "{}{}AC{}{}  {t: >7} ms",
+                style::Bold,
+                color::Fg(color::Green),
+                color::Fg(color::Reset),
+                style::Reset,
+            ),
+            JudgeStatus::Tle(t) => write!(
+                f,
+                "{}{}TLE{}{}  {t: >7} ms",
+                style::Bold,
+                color::Fg(color::Yellow),
+                color::Fg(color::Reset),
+                style::Reset,
+            ),
+            JudgeStatus::Wa(t) => write!(
+                f,
+                "{}{}WA{}{}  {t: >7} ms",
+                style::Bold,
+                color::Fg(color::Yellow),
+                color::Fg(color::Reset),
+                style::Reset,
+            ),
+            JudgeStatus::Wj => write!(f, "..."),
+        }
+    }
+}
+
+impl JudgeStatus {
+    fn to_string_with_emoji(&self) -> String {
+        match self {
+            &JudgeStatus::Ac(t) => format!("{} 🎉", JudgeStatus::Ac(t)),
+            s => format!("{s}"),
+        }
+    }
+}
+
+fn statuses_to_string(judge_statuses: &[JudgeStatus], n: usize) -> String {
+    judge_statuses
+        .iter()
+        .enumerate()
+        .map(|(i, s)| {
+            if *s == JudgeStatus::Wj {
+                format!(
+                    "Test Case {}: {}\n",
+                    i + 1,
+                    iter::once(".").cycle().take(n).collect::<String>()
+                )
+            } else {
+                format!("Test Case {}: {s}\n", i + 1)
+            }
+        })
+        .collect()
+}
+
+fn overall_result(judge_statuses: &[JudgeStatus]) -> JudgeStatus {
+    let mut ac = false;
+    let mut tle = false;
+    let mut wa = false;
+    let mut time = 0;
+    for s in judge_statuses {
+        match *s {
+            JudgeStatus::Ac(t) => {
+                ac = true;
+                time = time.max(t);
+            }
+            JudgeStatus::Tle(t) => {
+                tle = true;
+                time = time.max(t);
+            }
+            JudgeStatus::Wa(t) => {
+                wa = true;
+                time = time.max(t);
+            }
+            JudgeStatus::Wj => panic!(),
+        }
+    }
+    match (ac, tle, wa) {
+        (_, _, true) => JudgeStatus::Wa(time),
+        (_, true, false) => JudgeStatus::Tle(time),
+        (true, false, false) => JudgeStatus::Ac(time),
+        (false, false, false) => panic!(),
+    }
+}
+
+async fn display_result(mut receiver: Receiver<ReternMessage>) {
+    let number_of_cases = match receiver.recv().await {
+        Some(ReternMessage::NumberOfTestCases { n }) => n,
+        Some(ReternMessage::NotSuchProblem { problem_number }) => {
+            println!("Not such problem: {problem_number}");
+            return;
+        }
+        r => panic!("{:?}", r),
+    };
+    let mut judge_statuses = vec![JudgeStatus::Wj; number_of_cases];
+    let mut old = String::new();
+    for i in (0..4).cycle() {
+        match receiver.try_recv() {
+            Ok(ReternMessage::SubmissionResult {
+                test_case_number,
+                result,
+                time,
+                killed,
+            }) => {
+                judge_statuses[test_case_number] = if killed {
+                    JudgeStatus::Tle(time)
+                } else if result {
+                    JudgeStatus::Ac(time)
+                } else {
+                    JudgeStatus::Wa(time)
+                };
+            }
+            Ok(ReternMessage::Close) => {
+                break;
+            }
+            _ => (),
+        }
+        let s = statuses_to_string(&judge_statuses, i);
+        print!("{}{}", Erase(&old), s);
+        old = s;
+        tokio::time::sleep(std::time::Duration::from_millis(200)).await;
+    }
+    println!(
+        "\nResult: {}",
+        overall_result(&judge_statuses).to_string_with_emoji()
+    );
 }
